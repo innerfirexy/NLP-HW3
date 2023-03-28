12fda3a4628126171044367545f26b2e09b9e03b
shawntai <shawn890715@gmail.com>
2023-03-28 17:51:30 -0400

    formatting

--- decoder.py
+++ decoder.py
@@ -8,49 +8,54 @@ import keras
 
 from extract_training_data import FeatureExtractor, State
 
-class Parser(object): 
 
+class Parser(object):
     def __init__(self, extractor, modelfile):
         self.model = keras.models.load_model(modelfile)
         self.extractor = extractor
-        
+
         # The following dictionary from indices to output actions will be useful
-        self.output_labels = dict([(index, action) for (action, index) in extractor.output_labels.items()])
+        self.output_labels = dict(
+            [(index, action) for (action, index) in extractor.output_labels.items()]
+        )
 
     def parse_sentence(self, words, pos):
-        state = State(range(1,len(words)))
-        state.stack.append(0)    
+        state = State(range(1, len(words)))
+        state.stack.append(0)
 
-        while state.buffer: 
+        while state.buffer:
             pass
-            # TODO: Write the body of this loop for part 4 
+            # TODO: Write the body of this loop for part 4
 
         result = DependencyStructure()
-        for p,c,r in state.deps: 
-            result.add_deprel(DependencyEdge(c,words[c],pos[c],p, r))
-        return result 
-        
+        for p, c, r in state.deps:
+            result.add_deprel(DependencyEdge(c, words[c], pos[c], p, r))
+        return result
+
 
 if __name__ == "__main__":
 
-    WORD_VOCAB_FILE = 'data/words.vocab'
-    POS_VOCAB_FILE = 'data/pos.vocab'
+    WORD_VOCAB_FILE = "data/words.vocab"
+    POS_VOCAB_FILE = "data/pos.vocab"
 
     try:
-        word_vocab_f = open(WORD_VOCAB_FILE,'r')
-        pos_vocab_f = open(POS_VOCAB_FILE,'r') 
+        word_vocab_f = open(WORD_VOCAB_FILE, "r")
+        pos_vocab_f = open(POS_VOCAB_FILE, "r")
     except FileNotFoundError:
-        print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
-        sys.exit(1) 
+        print(
+            "Could not find vocabulary files {} and {}".format(
+                WORD_VOCAB_FILE, POS_VOCAB_FILE
+            )
+        )
+        sys.exit(1)
 
     extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
     parser = Parser(extractor, sys.argv[1])
 
-    with open(sys.argv[2],'r') as in_file: 
+    with open(sys.argv[2], "r") as in_file:
         for dtree in conll_reader(in_file):
             words = dtree.words()
             pos = dtree.pos()
             deps = parser.parse_sentence(words, pos)
             print(deps.print_conll())
             print()
-        
