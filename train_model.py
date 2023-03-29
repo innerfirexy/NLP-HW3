ecae4c11c44b6c0dd95e83be18159986726a67cd
shawntai <shawn890715@gmail.com>
2023-03-28 23:50:22 -0400

    Part 3

--- train_model.py
+++ train_model.py
@@ -8,14 +8,18 @@ from keras.layers import Flatten, Embedding, Dense
 
 def build_model(word_types, pos_types, outputs):
     # TODO: Write this function for part 3
+
     model = Sequential()
-    # model.add(...)
+    model.add(Embedding(word_types, 32, input_length=6))
+    model.add(Flatten())
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(10, activation="relu"))
+    model.add(Dense(outputs, activation="softmax"))
     model.compile(keras.optimizers.Adam(lr=0.01), loss="categorical_crossentropy")
     return model
 
 
 if __name__ == "__main__":
-
     WORD_VOCAB_FILE = "data/words.vocab"
     POS_VOCAB_FILE = "data/pos.vocab"
 
